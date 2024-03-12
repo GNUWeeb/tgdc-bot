dd63bb81d9ef44df7f347f579e0aaa5415460533
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-03-12 21:50:35 +0700

    clean up
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,12 +4,12 @@ set -e;
 
 cd /var/www/tgdc-bot/;
 
-apt update -y;
-apt install -y build-essential cmake curl wget autoconf make;
+apt-get update -y;
+apt-get install -y build-essential cmake curl wget autoconf make;
 
 mkdir -pv build;
 cd build;
-cmake -DCMAKE_BUILD_TYPE=Release modules/td;
+cmake -DCMAKE_BUILD_TYPE=Release ../modules/td;
 cmake --build .;
 
 cd /;
