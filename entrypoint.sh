a85d6bf0aebd8865c36b40a5717ce941c37c4d7e
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-03-13 05:21:24 +0700

    clean up
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,15 +2,21 @@
 
 set -e;
 
-cd /var/www/tgdc-bot/;
-mkdir -pv build/tmp/td;
-mkdir -pv build/lib/td;
-
-cd build/tmp/td;
 export CC=clang-18;
 export CXX=clang++-18;
 
+cd /var/www/tgdc-bot/;
+mkdir -pv build/tmp/td;
+mkdir -pv build/lib/td;
+mkdir -pv build/tmp/tgbot-api;
+
+cd build/tmp/td;
 cmake -DCMAKE_BUILD_TYPE=Release ../../../modules/td -DCMAKE_INSTALL_PREFIX:PATH=../../lib/td;
+cmake --build . --target install -j$(nproc);
+cd ../../..;
+
+cd build/tmp/tgbot-api;
+cmake -DCMAKE_BUILD_TYPE=Release ../../../modules/telegram-bot-api;
 cmake --build . --target install -j$(nproc);
 
 cd /;
