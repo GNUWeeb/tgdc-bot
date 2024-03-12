530720e5b1b3427d986158015d3792b105147b90
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-03-12 21:49:00 +0700

    build tdlib
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,16 @@
 #!/bin/bash
 
+set -e;
+
+cd /var/www/tgdc-bot/;
+
 apt update -y;
 apt install -y build-essential cmake curl wget autoconf make;
+
+mkdir -pv build;
+cd build;
+cmake -DCMAKE_BUILD_TYPE=Release modules/td;
+cmake --build .;
+
+cd /;
 exec /usr/local/bin/gwinitd;
