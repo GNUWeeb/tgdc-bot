666b95005bf877f12c73f79b0c1798c7143f9886
Ammar Faizi <ammarfaizi2@gnuweeb.org>
2024-03-12 22:01:58 +0700

    build with clang
    Signed-off-by: Ammar Faizi <ammarfaizi2@gnuweeb.org>

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,12 +5,20 @@ set -e;
 cd /var/www/tgdc-bot/;
 
 apt-get update -y;
-apt-get install -y build-essential cmake curl wget autoconf make gperf libssl-dev zlib1g-dev;
+apt-get install -y sudo build-essential cmake curl wget autoconf make gperf libssl-dev zlib1g-dev;
 
-mkdir -pv build;
-cd build;
-cmake -DCMAKE_BUILD_TYPE=Release ../modules/td;
-cmake --build .;
+wget https://apt.llvm.org/llvm.sh -O /tmp/llvm.sh;
+bash /tmp/llvm.sh 18;
+
+mkdir -pv build/tmp/tdlib;
+mkdir -pv build/lib/tdlib;
+
+cd build/tmp/tdlib;
+export CC=clang-18;
+export CXX=clang++-18;
+
+cmake -DCMAKE_BUILD_TYPE=Release ../../../modules/td -D -DCMAKE_INSTALL_PREFIX:PATH=../../tdlib;
+cmake --build . --target install -j$(nproc);
 
 cd /;
 exec /usr/local/bin/gwinitd;
